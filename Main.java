8b2a06db0cbd84fff2bf095d68dd85fa2cb388bc
Nicolo Ceneda <47401951+nicoloceneda@users.noreply.github.com>
2022-01-03 02:16:30 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+package tutorial;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/// Printing
+		System.out.println("Hello World!");
+		
+		// Variables and data types
+		int var_1 = 5;
+		double var_2 = 5.0;
+		boolean var_3 = true;
+		char var_4 = 'c';
+		String var_5 = "str";
+
+		// Operators
+		
+		
+				
+	}
+
+}
+
